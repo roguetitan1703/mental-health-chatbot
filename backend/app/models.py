a20962079aa977bc5ecf745bc32fb654029c79e6
roguetitan1703 <omchandel1703@gmail.com>
2024-09-25 06:19:38 +0530

    llama init

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,10 +9,10 @@ class TinyLlamaModel:
         self.filename = r"TinyLlama-Friendly-Psychotherapist.f16.gguf"
         # self.tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.filename, local_files_only=True)
         self.tokenizer = AutoTokenizer.from_pretrained(f"{self.model_id}\\saved_tokenizer")
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_id,gguf_file=self.filename,  local_files_only=True)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_id,gguf_file=self.filename, local_files_only=True)
         
          # Creating the pipeline for chat-like interaction
-        # self.pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
+        self.pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
 
     def generate_response(self, prompt):
         system_message = {
@@ -28,13 +28,14 @@ class TinyLlamaModel:
         ]
 
 
-        self.tokenized_chat = self.tokenizer.apply_chat_template(chat_history, chat_template="TinyLlama-1.1B-32k", tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        # self.tokenized_chat = self.tokenizer.apply_chat_template(chat_history, chat_template="TinyLlama-1.1B-32k", tokenize=True, add_generation_prompt=True, return_tensors="pt")
         
-        outputs = self.model.generate(self.tokenized_chat, max_new_tokens=128) 
+        # outputs = self.model.generate(self.tokenized_chat, max_new_tokens=128) 
 
         # Using the pipeline to generate response based on the chat history
-        # response = self.pipe(chat_history, max_length=1024, return_tensors="pt")["generated_text"][-1]
+        response = self.pipe(chat_history, max_length=1024, return_tensors="pt")["generated_text"][-1]
         # assistant_response = self.tokenizer.decode(response, skip_special_tokens=True)
 
         # return assistant_response
-        return self.tokenizer.decode(outputs[0])
+        return response
+        # return self.tokenizer.decode(outputs[0])
